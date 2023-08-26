526fa719b75a408b320b387c59ed0bab5be6e824
Saracen <SaracenOne@gmail.com>
2023-08-27 00:14:47 +0100

    Add missing files.

---
+++ TOMB4/tomb4/tomb4plus/teleporter.cpp
@@ -0,0 +1,22 @@
+#include "../../tomb4/pch.h"
+
+#include "../../global/types.h"
+#include "../../game/control.h"
+#include "../../game/lara.h"
+#include "../../game/camera.h"
+
+void ControlTeleporter(short item_number) {
+	ITEM_INFO* item = &items[item_number];
+
+	if (TriggerActive(item)) {
+		item->flags &= ~(IFL_CODEBITS);
+
+		lara_item->pos.x_pos = item->pos.x_pos;
+		lara_item->pos.y_pos = item->pos.y_pos;
+		lara_item->pos.z_pos = item->pos.z_pos;
+		lara_item->pos.y_rot = item->pos.y_rot + 0x8000;
+		lara_item->room_number = item->room_number;
+
+		camera.fixed_camera = 1;
+	}
+}
